ef3ca1f4ece836f3fe8f2021c726f64b1b52b350
gambo94 <fedigambo94@gmail.com>
2021-05-18 17:16:40 +0200

    added style and imagelink component and rank component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React, { component } from 'react';
 import './App.css';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js';
+
+const options = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      },
+      resize: true
+    }
+  }
+}
 
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles"
+                params={options} />
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */}
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
